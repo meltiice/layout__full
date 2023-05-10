1dc56d500a7d6e3d07c462d1af4b2eb4466e9ad3
Margarita Bershtainis <margarita.bershtainis@gmail.com>
2023-05-11 02:00:00 +0300

    commit 2 - the best sequel

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,39 @@
-import '../scss/style.scss'
+import '../scss/style.scss';
 
-console.log('Works!')
+console.log('Works!');
+var count = 0;
+var readmore = document.querySelector("#readmore");
+var logos = document.querySelector("#logos");
+console.log('Works2!');
+readmore.addEventListener('click', function (e) {
+   e.preventDefault();
+   logos.classList.toggle('open-content--show');
+   readmore.classList.toggle('read-more-rotate');
+   count++;
+   if (count % 2 === 1) {
+      readmore.innerHTML = "Скрыть";
+   }
+   else {
+      readmore.innerHTML = "Показать всё";
+   }
+});
+console.log('Works3!');
+const swiper = new Swiper('.swiper-container', {
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+   },
+   enabled: true,
+   width: 256
+});
+console.log('Works4!');
+/*
+console.log(window.innerWidth); 
+if(window.innerWidth <= 768) {
+   swiper.enable();
+}
+else {
+   swiper.destroy(false, false);
+}
+console.log("Swiper done");
+*/
